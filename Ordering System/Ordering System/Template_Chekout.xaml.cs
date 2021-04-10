98037ce7beba97bf0170b3ba598759b870aa6a8c
Steven <Stevenmlilo@gmail.com>
2021-04-09 19:00:56 -0600

    All templates are done, I will now start doing most of the scenarios - I made these to just copy and paste, instead of recoding each new scenario

---
+++ Ordering System/Ordering System/Template_Chekout.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ordering_System
+{
+    /// <summary>
+    /// Interaction logic for Template_Chekout.xaml
+    /// </summary>
+    public partial class Template_Chekout : UserControl
+    {
+        public Template_Chekout()
+        {
+            InitializeComponent();
+        }
+        /*private void Help_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Select your method of payment by choosing cash or credit/debit." + "\n" + "If you wish to add more items to your order, press Return to Menu.");
+        }
+
+        private void Cash_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Please wait as a staff member will arrive with your bill shortly.");
+        }
+
+        private void CreditDebit_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Please wait as a staff member will arrive with your bill shortly.");
+         */
+    }
+}
